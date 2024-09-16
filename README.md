7a714397da7cc1e29265dc777abf663329a83fab
Werner Robitza <slhck@users.noreply.github.com>
2024-09-16 09:21:42 +0200

    refactor: major refactor (#2)
    This adds new features like:
    
    - Handling of multiple repos
    - Interactive repo selection
    
    Development improvements:
    
    - Test suite using vitest
    - Consistent formatting using biome

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
-- [Usage](#usage)
-  - [List Container Repositories for entire instance, groups or projects](#list-container-repositories-for-entire-instance-groups-or-projects)
-  - [Cleanup Container Repositories](#cleanup-container-repositories)
-  - [How do I known my Container Repository ID?](#how-do-i-known-my-container-repository-id)
-  - [Example keep/delete regex](#example-keepdelete-regex)
-- [How does cleanup work?](#how-does-cleanup-work)
-- [License](#license)
-
 # GitLab Container Registry cleaner
 
 CLI to list and cleanup your GitLab Container Registry and Container Repositories.
 
-## Usage
+Contents:
 
-Clone this repository and run:
+- [What this is?](#what-this-is)
+- [Requirements](#requirements)
+- [Usage](#usage)
+  - [List Container Repositories for entire instance, groups or projects](#list-container-repositories-for-entire-instance-groups-or-projects)
+  - [Cleanup Container Repositories](#cleanup-container-repositories)
+  - [How do I know my Container Repository ID?](#how-do-i-know-my-container-repository-id)
+  - [Example Options](#example-options)
+  - [How does cleanup work?](#how-does-cleanup-work)
+- [Development](#development)
+- [Testing](#testing)
+- [License](#license)
 
-```sh
-npm install
-./gitlab-container-registry-cleaner --help
-```
+## What this is?
 
-_Note: I can publish this as a package in `npm` public registry if someone deems it useful, just ask for it :)_
+This tool helps you clean up the GitLab Container Registry by deleting tags that match a specified regex pattern.
 
-Requirements:
+- Interactive repository selection
+- Multiple repository deletion in a single command
+- Added option to keep/delete tags by age
+- More verbose logging
+- And: unit testing, code cleanup/formatting and refactoring
+
+## Requirements
 
 - Node 18+
 - GitLab token with `api` scope
@@ -29,25 +34,49 @@ Requirements:
 Export environment variables to specify your GitLab server and token:
 
 ```sh
-export GITLAB_HOST=https://gitlab.awesomecompany.net
-export GITLAB_TOKEN="xxx" 
+export GITLAB_HOST=https://gitlab.example.com
+export GITLAB_TOKEN="xxx"
+```
+
+Alternatively, you can use a `.env` file to store those variables:
+
+```sh
+GITLAB_HOST=https://gitlab.example.com
+GITLAB_TOKEN="xxx"
+```
+
+## Usage
+
+Clone this repository and run:
+
+```sh
+npm i -g yarn
+yarn
+yarn build
+./gitlab-container-registry-cleaner --help
 ```
 
 ### List Container Repositories for entire instance, groups or projects
 
-List instance-wide Container Repositories:
+The following command will list all instance-wide container repositories, checking 1 to 10000 repository IDs with a concurrency of 20 by default:
 
 ```sh
-# Check all Container Repository from 1 to 10000 with a concurrency of 20 by default
 ./gitlab-container-registry-cleaner list all
+```
 
-# Customize concurrency and ID range
-# Example to check between ID 500-1000 with 10 concurrency (up to 10 requests in parallel)
-# Output result to /tmp/repositories.json
+You can output them to a file so you can later look at the repository IDs:
+
+```sh
+./gitlab-container-registry-cleaner list all -o /tmp/repositories.json
+```
+
+You can also filter the repositories by providing a start and end ID, and a concurrency level. The concurrency level is the number of concurrent requests to GitLab API, and the below example checks 500 to 1000 with 10 concurrency (up to 10 requests in parallel):
+
+```sh
 ./gitlab-container-registry-cleaner list all -s 500 -e 1000 -c 10 -o /tmp/repositories.json
 ```
 
-Listing instance wide Container Repositories is done concurrently and may take some time. Internally, it calls GitLab REST API to check every repository ID between 1 and 10000 by default. 
+Listing instance wide Container Repositories is done concurrently and may take some time. Internally, it calls GitLab REST API to check every repository ID between 1 and 10000 by default.
 
 You can also list per group or per project:
 
@@ -61,7 +90,7 @@ You can also list per group or per project:
 
 ### Cleanup Container Repositories
 
-Run cleanup for a project's Container Repository. Note that by default **cleanup will dry-run and regex won't match anything for safety reasons**. 
+Run cleanup for a project's Container Repository. Note that by default **cleanup will dry-run and regex won't match anything for safety reasons**.
 
 Example usage:
 
@@ -80,41 +109,97 @@ cat /tmp/tags.json | jq '.[] | .name + "\t" + .created_at ' -r
 ./gitlab-container-registry-cleaner clean 161 -k 'v?[0-9]+\.[0-9]+\.[0-9]+.*' -d '.*' --no-dry-run
 ```
 
-### How do I known my Container Repository ID? 
+### How do I know my Container Repository ID?
 
-Repository ID is visible in the URL when navigating to your project Container Repository in _Deploy > Container Registry > [repository name]_. URL looks like:
+The Repository ID is visible in the URL when navigating to your project Container Repository in _Deploy > Container Registry > [repository name]_. The URL looks like:
 
 ``` sh
 # Repository ID is 42
 https://gitlab.mycompany.net/somegroup/myproject/container_registry/42
 ```
 
-Alternatively, use `./gitlab-container-registry-cleaner list` (see above)
+Note that this ID is not the same as the project ID!
 
-### Example keep/delete regex
+Alternatively, use `./gitlab-container-registry-cleaner list` (see above) to generate the JSON file. Then you can pass that file to the clean command without any ID, which will show a list of repositories to choose from:
 
 ```sh
-# Keep releases and remove everything else
+./gitlab-container-registry-cleaner list all -o /tmp/repositories.json
+./gitlab-container-registry-cleaner clean -j /tmp/repositories.json
+```
+
+This might show something like the following, where you can select the repositories you want to clean:
+
+```
+? Select a repository to clean: (Press <space> to select, <a> to toggle all, <i> to invert selection, and <enter> to proceed)
+ firstgroup
+❯◯ foo/bar/example-project
+ secondgroup
+ ◯ analytics-project/apm-server
+ ◯ analytics-project/elasticsearch
+ ◯ analytics-project/grafana
+ ◯ analytics-project/kibana
+ ◯ analytics-project/logstash
+ ◯ analytics-project/prometheus
+(Use arrow keys to reveal more choices)
+```
+
+Press `enter` to clean the selected repositories.
+
+### Example Options
+
+Keep releases and remove everything else
+
+```sh
 -k 'v?[0-9]+\.[0-9]+\.[0-9]+.*' -d '.*'
+```
 
-# Delete all tags starting with `dev-`
-# '$^' won't match anything
+Delete all tags starting with `dev-`. `$^` won't match anything:
+
+```sh
 -k '$^' -d '$dev-.*'
+```
 
-# Delete all tags - USE WITH CARE
+Delete all tags - USE WITH CARE:
+
+```sh
 -k '$^' -d '.*'
 ```
 
-## How does cleanup work?
+Delete all, but keep the most recent 10 tags:
+
+```sh
+-k '$^' -d '.*' -n 10
+```
+
+Keep the most recent 5 tags and delete everything older than 30 days:
+
+```sh
+-k '$^' -d '.*' -n 5 -a 30
+```
+
+### How does cleanup work?
 
 Cleanup behavior is similar to [GitLab Registry cleanup policy](https://docs.gitlab.com/ee/user/packages/container_registry/reduce_container_registry_storage.html#how-the-cleanup-policy-works). Cleaner calls GitLab REST API concurrently such as:
 
 - List all tags in repository with [_List registry repository tags_](https://docs.gitlab.com/ee/api/container_registry.html#list-registry-repository-tags)
 - Filter out tags not matching keep regex, filter in tags matching delete regex
 - Get tag details with [_Get details of a registry repository tag_](https://docs.gitlab.com/ee/api/container_registry.html#get-details-of-a-registry-repository-tag) as List registry repository tags only provide tag name, not creation date
-- Filter in tags older than specified number of days 
+- Filter in tags older than specified number of days
+- Keep the most recent N tags if specified
 - Delete 'em !
+
+## Development
+
+Run `yarn install` to install dependencies.
+
+Run `yarn dev -- <command>` to start the dev version of the CLI with a given command.
+
+Run `yarn build` to build the project into `dist`.
+
+## Testing
+
+Run `yarn test` to run the tests.
 
 ## License
 
-[Apache License](./LICENSE.txt)
+Licensed under Apache License 2.0, see [LICENSE.txt](./LICENSE.txt) for more details.
---
+++ test/cleaner.test.ts
@@ -0,0 +1,391 @@
+import type {
+	CondensedProjectSchema,
+	CondensedRegistryRepositoryTagSchema,
+	RegistryRepositorySchema,
+	RegistryRepositoryTagSchema,
+} from "@gitbeaker/rest";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+	beforeEach,
+	describe,
+	expect,
+	it,
+	vi,
+} from "vitest";
+import { GitLabContainerRepositoryCleaner } from "../src/cleaner.js";
+
+describe("Cleaner", () => {
+	const _fakeTag = (name: string, projectId: number, createdAt?: string) => ({
+		name,
+		path: `my-group/my-project-${projectId}`,
+		location: `gitlab.com/my-group/my-project-${projectId}`,
+		revision: "XXXX",
+		short_revision: "XXXX",
+		digest: "XXXX",
+		created_at: "2024-01-01T00:00:00Z",
+		total_size: 1000,
+		...(createdAt && { created_at: createdAt }),
+	});
+
+	const _condensedTag = (
+		tag: RegistryRepositoryTagSchema,
+	): CondensedRegistryRepositoryTagSchema => ({
+		name: tag.name,
+		path: tag.path,
+		location: tag.location,
+	});
+
+	const FAKE_PROJECTS: CondensedProjectSchema[] = [
+		...[1, 2, 3].map(
+			(i) =>
+				({
+					id: i,
+					description: `Project ${i}`,
+					name: `Project ${i}`,
+					path_with_namespace: `my-group/my-project-${i}`,
+					visibility: "public",
+					web_url: `https://gitlab.com/my-group/my-project-${i}`,
+					path: `my-group/my-project-${i}`,
+				}) as CondensedProjectSchema,
+		),
+	];
+
+	const FAKE_TAGS: Record<number, RegistryRepositoryTagSchema[]> = {
+		1: [
+			_fakeTag("v1.0.0", 1, "2024-01-01T00:00:00Z"),
+			_fakeTag("v1.1.0", 1, "2024-02-01T00:00:00Z"),
+			_fakeTag("v1.2.0", 1, "2024-03-01T00:00:00Z"),
+			_fakeTag("v1.3.0", 1, "2024-04-01T00:00:00Z"),
+			_fakeTag("v1.4.0", 1, "2024-05-01T00:00:00Z"),
+		],
+		2: [
+			// unused
+		],
+		3: [
+			_fakeTag("release-1.0.0", 3, "2024-01-01T00:00:00Z"),
+			_fakeTag("dev-build-123", 3, "2024-02-01T00:00:00Z"),
+			_fakeTag("release-1.1.0", 3, "2024-03-01T00:00:00Z"),
+			_fakeTag("dev-build-456", 3, "2024-04-01T00:00:00Z"),
+		],
+	} as const;
+
+	beforeAll(() => {
+		vi.useFakeTimers();
+		vi.setSystemTime("2024-06-01T00:00:00Z");
+	});
+
+	afterAll(() => {
+		vi.useRealTimers();
+	});
+
+	let cleaner: GitLabContainerRepositoryCleaner;
+	let currentFakeProjects: CondensedProjectSchema[] = [];
+	let currentFakeTags: Record<number, RegistryRepositoryTagSchema[]> = {};
+
+	beforeEach(() => {
+		currentFakeProjects = [...FAKE_PROJECTS];
+		currentFakeTags = { ...FAKE_TAGS };
+		cleaner = new GitLabContainerRepositoryCleaner({
+			gitlabHost: "https://gitlab.com",
+			gitlabToken: "token",
+			dryRun: true,
+			verbose: true,
+		});
+	});
+
+	const handlers = [
+		http.get("https://gitlab.com/api/v4/projects", () => {
+			return HttpResponse.json(FAKE_PROJECTS);
+		}),
+		http.get(
+			"https://gitlab.com/api/v4/registry/repositories/:repositoryId",
+			({ params }) => {
+				const repoId = params.repositoryId as unknown as number;
+				return HttpResponse.json({
+					id: repoId,
+					name: `Project ${repoId}`,
+					path_with_namespace: `my-group/my-project-${repoId}`,
+					visibility: "public",
+					path: `my-group/my-project-${repoId}`,
+					project_id: repoId, // for simplicity, we use the repo id as the project id
+					location: `gitlab.com/my-group/my-project-${repoId}`,
+					created_at: "2024-01-01T00:00:00Z",
+					cleanup_policy_started_at: "2024-01-02T00:00:00Z",
+					// ...(params.tagsCount && {
+					// 	tags_count: FAKE_TAGS[repoId]?.length ?? 0,
+					// }),
+					tags_count: FAKE_TAGS[repoId]?.length ?? 0,
+					...(params.tags && {
+						tags: FAKE_TAGS[repoId]?.map(_condensedTag) ?? [],
+					}),
+				} as RegistryRepositorySchema);
+			},
+		),
+		http.get(
+			"https://gitlab.com/api/v4/projects/:projectId/registry/repositories/:repositoryId/tags",
+			({ params }) => {
+				const projectId = Number(params.projectId);
+				return HttpResponse.json(
+					FAKE_TAGS[projectId]?.map(_condensedTag) ?? [],
+				);
+			},
+		),
+		http.get(
+			"https://gitlab.com/api/v4/projects/:projectId/registry/repositories/:repositoryId/tags/:tagName",
+			({ params }) => {
+				const projectId = Number(params.projectId);
+				const tagName = params.tagName as string;
+				const tag = FAKE_TAGS[projectId]?.find((t) => t.name === tagName);
+				if (tag) {
+					return HttpResponse.json(tag);
+				}
+				return new HttpResponse(null, { status: 404 });
+			},
+		),
+		http.delete(
+			"https://gitlab.com/api/v4/projects/:projectId/registry/repositories/:repositoryId/tags/:tagName",
+			({ params }) => {
+				const projectId = Number(params.projectId);
+				const tagName = params.tagName as string;
+				const tag = FAKE_TAGS[projectId]?.find((t) => t.name === tagName);
+				if (tag !== undefined) {
+					// biome-ignore lint/style/noNonNullAssertion: <explanation>
+					currentFakeTags[projectId] = currentFakeTags[projectId]!.filter(
+						(t) => t.name !== tagName,
+					);
+					return new HttpResponse(null, { status: 204 });
+				}
+				return new HttpResponse(null, { status: 404 });
+			},
+		),
+	];
+
+	const server = setupServer(...handlers);
+
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
+	// Helper method for checking expected and not expected tags
+	const checkExpectedAndNotExpectedTags = (
+		tagsToDelete: RegistryRepositoryTagSchema[],
+		expectedTags: RegistryRepositoryTagSchema[],
+		notExpectedTags: RegistryRepositoryTagSchema[],
+	) => {
+		for (const expectedTag of expectedTags) {
+			expect(tagsToDelete).toContainEqual(
+				expect.objectContaining({
+					name: expectedTag.name,
+					created_at: expectedTag.created_at,
+				}),
+			);
+		}
+
+		for (const notExpectedTag of notExpectedTags) {
+			expect(tagsToDelete).not.toContainEqual(
+				expect.objectContaining({
+					name: notExpectedTag.name,
+					created_at: notExpectedTag.created_at,
+				}),
+			);
+		}
+	};
+
+	it("should getContainerRepositoriesConcurrently", async () => {
+		const cleaner = new GitLabContainerRepositoryCleaner({
+			gitlabHost: "https://gitlab.com",
+			gitlabToken: "token",
+		});
+		const repositories = await cleaner.getContainerRepositoriesConcurrently(
+			1,
+			5,
+		);
+		expect(repositories).toBeDefined();
+		expect(repositories.length).toBeGreaterThan(0);
+	});
+
+	it("should keep the last N tags when cleaning up", async () => {
+		// @ts-expect-error Private method
+		const spy = vi.spyOn(cleaner, "deleteTagsConcurrently");
+
+		await cleaner.cleanupContainerRepositoryTags(1, {
+			keepMostRecentN: 2,
+			olderThanDays: 0,
+			keepRegex: "^$",
+			deleteRegex: ".*",
+		});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		// @ts-expect-error Undefined type
+		const [projectId, repositoryId, tagsToDelete] = spy.mock.calls[0];
+
+		expect(projectId).toBe(1);
+		expect(repositoryId).toBe(1);
+		expect(tagsToDelete).toHaveLength(3);
+
+		const expectedTags = [
+			FAKE_TAGS[1]?.[0],
+			FAKE_TAGS[1]?.[1],
+			FAKE_TAGS[1]?.[2],
+		] as RegistryRepositoryTagSchema[];
+
+		const notExpectedTags = [
+			FAKE_TAGS[1]?.[3],
+			FAKE_TAGS[1]?.[4],
+		] as RegistryRepositoryTagSchema[];
+
+		checkExpectedAndNotExpectedTags(
+			tagsToDelete,
+			expectedTags,
+			notExpectedTags,
+		);
+	});
+
+	it("should delete tags older than specified days", async () => {
+		// @ts-expect-error Private method
+		const spy = vi.spyOn(cleaner, "deleteTagsConcurrently");
+
+		await cleaner.cleanupContainerRepositoryTags(1, {
+			keepMostRecentN: 0,
+			olderThanDays: 45,
+			keepRegex: "^$",
+			deleteRegex: ".*",
+		});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		const [projectId, repositoryId, tagsToDelete] = spy.mock.calls[0];
+
+		expect(projectId).toBe(1);
+		expect(repositoryId).toBe(1);
+		expect(tagsToDelete).toHaveLength(4);
+
+		const expectedTags = [
+			FAKE_TAGS[1]?.[0],
+			FAKE_TAGS[1]?.[1],
+			FAKE_TAGS[1]?.[2],
+			FAKE_TAGS[1]?.[3],
+		] as RegistryRepositoryTagSchema[];
+
+		const notExpectedTags = [
+			FAKE_TAGS[1]?.[4],
+
+		] as RegistryRepositoryTagSchema[];
+
+		checkExpectedAndNotExpectedTags(
+			tagsToDelete,
+			expectedTags,
+			notExpectedTags,
+		);
+	});
+
+	it("should keep tags matching the keep regex", async () => {
+		// @ts-expect-error Private method
+		const spy = vi.spyOn(cleaner, "deleteTagsConcurrently");
+
+		await cleaner.cleanupContainerRepositoryTags(3, {
+			keepMostRecentN: 0,
+			olderThanDays: 0,
+			keepRegex: "release-.*",
+			deleteRegex: ".*",
+		});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		const [projectId, repositoryId, tagsToDelete] = spy.mock.calls[0];
+
+		expect(projectId).toBe(3);
+		expect(repositoryId).toBe(3);
+		expect(tagsToDelete).toHaveLength(2);
+
+		const expectedTags = [
+			FAKE_TAGS[3]?.[1],
+			FAKE_TAGS[3]?.[3],
+		] as RegistryRepositoryTagSchema[];
+
+		const notExpectedTags = [
+			FAKE_TAGS[3]?.[0],
+			FAKE_TAGS[3]?.[2],
+		] as RegistryRepositoryTagSchema[];
+
+		checkExpectedAndNotExpectedTags(
+			tagsToDelete,
+			expectedTags,
+			notExpectedTags,
+		);
+	});
+
+	it("should delete tags matching the delete regex", async () => {
+		// @ts-expect-error Private method
+		const spy = vi.spyOn(cleaner, "deleteTagsConcurrently");
+
+		await cleaner.cleanupContainerRepositoryTags(1, {
+			keepMostRecentN: 0,
+			olderThanDays: 0,
+			keepRegex: "^$",
+			deleteRegex: "v1\\.[13]\\.0",
+		});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		const [projectId, repositoryId, tagsToDelete] = spy.mock.calls[0];
+
+		expect(projectId).toBe(1);
+		expect(repositoryId).toBe(1);
+		expect(tagsToDelete).toHaveLength(2);
+
+		const expectedTags = [
+			FAKE_TAGS[1]?.[1],
+			FAKE_TAGS[1]?.[3],
+		] as RegistryRepositoryTagSchema[];
+
+		const notExpectedTags = [
+			FAKE_TAGS[1]?.[0],
+			FAKE_TAGS[1]?.[2],
+			FAKE_TAGS[1]?.[4],
+		] as RegistryRepositoryTagSchema[];
+
+		checkExpectedAndNotExpectedTags(
+			tagsToDelete,
+			expectedTags,
+			notExpectedTags,
+		);
+	});
+
+	it("should keep tags matching the keep regex and delete the rest", async () => {
+		// @ts-expect-error Private method
+		const spy = vi.spyOn(cleaner, "deleteTagsConcurrently");
+
+		await cleaner.cleanupContainerRepositoryTags(1, {
+			keepMostRecentN: 1,
+			olderThanDays: 30,
+			keepRegex: "v1\\.[02]\\.0",
+			deleteRegex: ".*",
+		});
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		const [projectId, repositoryId, tagsToDelete] = spy.mock.calls[0];
+
+		expect(projectId).toBe(1);
+		expect(repositoryId).toBe(1);
+		expect(tagsToDelete).toHaveLength(2);
+
+		const expectedTags = [
+			FAKE_TAGS[1]?.[1],
+			FAKE_TAGS[1]?.[3],
+		] as RegistryRepositoryTagSchema[];
+
+		const notExpectedTags = [
+			FAKE_TAGS[1]?.[0],
+			FAKE_TAGS[1]?.[2],
+			FAKE_TAGS[1]?.[4],
+		] as RegistryRepositoryTagSchema[];
+
+		checkExpectedAndNotExpectedTags(
+			tagsToDelete,
+			expectedTags,
+			notExpectedTags,
+		);
+	});
+});
